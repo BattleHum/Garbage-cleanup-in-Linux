c936987f769eafb2cd274dc30750a94187c1bb9f
BattleHum <111016119+BattleHum@users.noreply.github.com>
2024-03-23 20:53:13 +0200

    Update clearsystem.py

--- clearsystem.py
+++ clearsystem.py
@@ -1,12 +1,19 @@
-#!/bin/python3
+!/bin/python3
 import subprocess
 
-def clear():
+def upgrade():
+    print("Upgrade system...")
+    subprocess.call(["apt-get", "update", "-y"])
+    subprocess.call(["apt-get", "upgrade", "-y"])
+    subprocess.call(["apt-get", "dist-upgrade", "-y"])
+    print("You kali upgrade!")
+
+def clean():
     print("Clear system")
     subprocess.call(["apt-get", "clean", "-y"])
     subprocess.call(["apt-get", "autoremove", "-y"])
     print("System clear!")
-
-
+    
 if __name__ == '__main__':
-    clear()
+    upgrade()
+    clean()
